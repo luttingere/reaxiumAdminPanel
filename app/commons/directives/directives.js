64e81ee54f257395e4501f2cb25098c72fe7c9d8
luttingere <luttingere@gmail.com>
2016-04-06 08:47:02 -0430

    User module modifications

--- app/commons/directives/directives.js
+++ app/commons/directives/directives.js
@@ -25,23 +25,6 @@ angular.module('App')
             }
 
         }
-    })
+    });
 
-    .directive('dataTable', function () {
-        return {
-            link: function ($scope, element, attrs) {
-                $scope.$on('dataloaded', function () {
-                    console.log("Si funciona viejo");
-                    //$('#userTable').DataTable({
-                    //    "paging": true,
-                    //    "lengthChange": true,
-                    //    "searching": true,
-                    //    "ordering": true,
-                    //    "info": true,
-                    //    "autoWidth": true
-                    //});
-                });
-            }
-        };
-    })
 
--- app/users/services/UserServices.js
+++ app/users/services/UserServices.js
@@ -2,4 +2,23 @@
  * Created by Eduardo Luttinger on 05/04/2016.
  */
 angular.module('Users')
-.service('UserService');
+
+    .factory('UserLookup', function ($http) {
+        var lookup = {};
+        lookup.allUsers = function () {
+            return $http({
+                method: 'GET',
+                url: 'http://54.200.133.84/reaxium/Users/allUsersInfo',
+            }).then(function (response) {
+                var jsonObj = response.data;
+                return jsonObj.ReaxiumResponse.object;
+            });
+        };
+        return lookup;
+    })
+
+    .service('UserService', function (UserLookup) {
+        this.getUsers = function(){
+            return UserLookup.allUsers();
+        };
+    });

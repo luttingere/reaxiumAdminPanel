719ff6511d23bce296ff2fdf393f0783b09f46c6
vifg2184 <vladimir.fernandez21@gmail.com>
2016-06-30 14:12:49 -0400

    Mejora de textos en el panel de busqueda de rutas por dispositivo  Issue #32

--- app/app.js
+++ app/app.js
@@ -18,12 +18,25 @@ angular.module('App', ['ui.router',
 
 
     //Aqui lo primero que se ejecuta en angular como el document Ready en jquery
-    .run(['$templateCache', '$rootScope','$interval', function ($templateCache, $rootScope,$interval) {
+    .run(['$templateCache', '$rootScope', '$interval', function ($templateCache, $rootScope, $interval) {
 
         //remover el cache de la aplicacion
-       /* $rootScope.$on('$destroy', function() {
-            $templateCache.removeAll();
+        /* $rootScope.$on('$destroy', function() {
+         $templateCache.removeAll();
+         });*/
+
+        /*$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+            // If the state we are headed to has cached template views
+            if (typeof (toState) !== 'undefined' && typeof (toState.views) !== 'undefined') {
+                // Loop through each view in the cached state
+                for (var key in toState.views) {
+                    // Delete templeate from cache
+                    console.log("Delete cached template: " + toState.views[key].templateUrl);
+                    $templateCache.remove(toState.views[key].templateUrl);
+                }
+            }
         });*/
+
 
         $templateCache.put('sort-by.html', '<a ng-click="sort(sortvalue)"><span ng-transclude=""></span><span ng-show="sortedby == sortvalue">&nbsp;&nbsp;<i ng-class="{true: \'fa fa-sort-up\', false: \'fa fa-sort-desc\'}[sortdir == \'asc\']"></i></span></a>');
         $templateCache.put('searchbox.tpl.html', '<input class="form-control" type="text" placeholder="Place an address...">');
@@ -31,7 +44,7 @@ angular.module('App', ['ui.router',
         $rootScope.welcomePage = 'app/home/views/content.html';
 
         //controllar el intervalo del proceso del tracking
-        $rootScope.$on('$stateChangeStart', function() {
+        $rootScope.$on('$stateChangeStart', function () {
             $interval.cancel(interval);
         });
 
